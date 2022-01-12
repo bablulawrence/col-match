e74afdd1012fdbe7f81e33aff1fd073ef828a19b
Bablu Lawrence <bablulawrence@outlook.com>
2022-01-12 11:51:06 +0000

    Added error handling

--- col_match.py
+++ col_match.py
@@ -5,7 +5,11 @@ from pyspark.sql.types import *
 spark = SparkSession.builder.appName('colMatch').getOrCreate()
 
 def getDatabases():
-    return spark.sql('SHOW DATABASES')
+    try: 
+        return spark.sql('SHOW DATABASES')
+    except Exception as e: 
+        logger("Unable to get databases")
+        raise e
 
 def getTables(databases):
     tableSchema = StructType([
@@ -13,24 +17,41 @@ def getTables(databases):
             StructField('tableName', StringType(), False),
             StructField('isTemporary', BooleanType(), False)
     ])
-    itr = databases.rdd.toLocalIterator()    
     tables = spark.createDataFrame([], tableSchema)
+    itr = databases.rdd.toLocalIterator()    
     for x in itr:
-        spark.sql(f"USE {x['databaseName']}")
-        table = spark.sql('SHOW TABLES')
-        tables = tables.union(table)
+        try: 
+            spark.sql(f"USE {x['databaseName']}")
+            table = spark.sql('SHOW TABLES')
+            tables = tables.union(table)
+        except Exception as e:
+            logger(f"Unable to list tables for database {x['databaseName']}")            
     return tables
 
 
-def getSchema(tables): 
+def getColumns(tables): 
+    columnSchema = StructType([
+            StructField('col_name', StringType(), False),
+            StructField('data_type', StringType(), False),
+            StructField('comment', StringType(), False),
+            StructField('database', StringType(), False),
+            StructField('tableName', StringType(), False),
+            StructField('isTemporary', BooleanType(), False)
+    ])
+    columns = spark.createDataFrame([], columnSchema)
     itr = tables.rdd.toLocalIterator()
-    for x in itr:
-        spark.sql(f"USE {x['database']}")
-        schema = spark.sql(f"DESCRIBE EXTENDED { x['tableName'] }")
-        schema.show()
+    try: 
+        for x in itr:
+            spark.sql(f"USE {x['database']}")
+            cols = spark.sql(f"DESCRIBE EXTENDED { x['tableName'] }")
+            cols = cols.withColumn('database', lit(x['database'])) \
+                        .withColumn('tableName', lit(x['tableName'])) \
+                        .withColumn('isTemporary', lit(x['isTemporary']))
+            columns = columns.union(cols)
+    except Exception as e:
+        logger(f"Unable to get columns for table {x['tableName']}")        
+    return columns
 
-tables = getTables(getDatabases())
-tables.show()
-
-getSchema(tables)
+columns = getColumns(getTables(getDatabases()))
+columns.show(100)
 
