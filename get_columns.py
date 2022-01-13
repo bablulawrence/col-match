51951ca69a76a5c3a471cd79ef8d4365c8d51cf2
Bablu Lawrence <bablulawrence@outlook.com>
2022-01-13 09:20:18 +0000

    Done with fuzzy match logic

--- get_columns.py
+++ get_columns.py
@@ -5,6 +5,8 @@ import sys
 from pyspark.sql.functions import * 
 from pyspark.sql import SparkSession
 from pyspark.sql.types import *
+from thefuzz import fuzz
+from thefuzz import process
 
 
 def getDatabases(spark, dbList):
@@ -39,11 +41,33 @@ def getTables(spark, databases, tableList):
     return tables
 
 
-def getColumns(spark, tables, columList): 
+def getColumns(spark, tables, columnList, fuzzyScorer=fuzz.ratio): 
+    
+    def getScore(columns):
+        
+        if (fuzzyScorer == 'partial'):
+            scorer = fuzz.partial_ratio
+        elif(fuzzyScorer == 'token-sort'):
+            scorer = fuzz.token_sort_ratio
+        elif(fuzzyScorer == 'token-set'):
+            scorer = fuzz.token_sort_ratio
+        else:
+            scorer = fuzz.ratio
+
+        maxScore = process.extractOne(columns, columnList, scorer=scorer)[1]
+        columns = process.extract(columns, columnList, scorer=scorer)
+        return { 'matchCols': json.dumps(columns), 'maxScore': maxScore}
+
+    schema = StructType([
+        StructField('matchCols', StringType(), False), 
+        StructField('maxScore', IntegerType(), False)
+    ])
+    getScoreUdf = udf(getScore, schema)
     columnSchema = StructType([
             StructField('col_name', StringType(), False),
             StructField('data_type', StringType(), False),
             StructField('comment', StringType(), False),
+            StructField('matchResult', schema, False),
             StructField('database', StringType(), False),
             StructField('tableName', StringType(), False),
             StructField('isTemporary', BooleanType(), False)
@@ -53,46 +77,51 @@ def getColumns(spark, tables, columList):
     try: 
         for x in itr:
             spark.sql(f"USE {x['database']}")
-            if not columList:
-                cols = spark.sql(f"DESCRIBE EXTENDED { x['tableName'] }")
+            if not columnList:
+                cols = spark.sql(f"DESCRIBE EXTENDED { x['tableName'] }") \
+                    .withColumn('maxResult', lit(None)) 
             else: 
-                cols = spark.sql(f"DESCRIBE EXTENDED { x['tableName'] }").filter(col('col_name').isin(columList))
+                cols = spark.sql(f"DESCRIBE EXTENDED { x['tableName'] }") \
+                    .withColumn('matchResult', getScoreUdf(col('col_name'))) 
             cols = cols.withColumn('database', lit(x['database'])) \
                         .withColumn('tableName', lit(x['tableName'])) \
                         .withColumn('isTemporary', lit(x['isTemporary']))
 
             columns = columns.union(cols)
+        columns = columns.select(col('col_name').alias('colName'), 
+                                 col('data_type').alias('dataType'),
+                                 'comment','database','tableName', 'isTemporary',
+                                 col('matchResult.maxScore').alias('maxScore'),
+                                 col('matchResult.matchCols').alias('matchCols'))
     except Exception as e:
-        logging.error(f"Unable to get columns for table {x['tableName']}")        
-    return columns
+        logging.error(f"Unable to get columns for table {x['tableName']}")
+    return columns.sort(col('maxScore').desc())
 
 def readParams(filePath):
     with open(filePath) as param_file:
        return json.load(param_file)
 
-# logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
-
 parser = argparse.ArgumentParser(description='Get columns',
- epilog="python get_columns.py --paramFilePath 'params.json' --outputFilePath 'dbfs:/mnt/data/get_columns/columns1.csv'" 
+ epilog="python get_columns.py --paramFilePath 'params.json' --outputFilePath 'dbfs:/mnt/data/get_columns/columns1.csv' --fuzzyScorer 'token-sort'" 
 )
 parser.add_argument('--paramFilePath', dest='paramFilePath', type=str, help='Path of the parameter file')
 parser.add_argument('--outputFilePath', dest='outputFilePath', type=str, help='Path of the output file')
+parser.add_argument('--fuzzyScorer', dest='fuzzyScorer', type=str, help='Fuzzy ratio type')
 args = parser.parse_args()
 
 params = readParams(args.paramFilePath)
 logging.info(json.dumps(params))
 
 spark = SparkSession.builder.appName('colMatch').getOrCreate()
-# dbs = getDatabases(spark, params['databases'])
-# dbs.show()
 databases =  getDatabases(spark, params['databases'])
 tables = getTables(spark, databases, params['tables'])
-columns = getColumns(spark, tables, params['columns'])
-columns.show(5)
+columns = getColumns(spark, tables, params['columns'], args.fuzzyScorer)
+columns.show(50, truncate=False)
 
 try: 
     columns.coalesce(1).write \
         .option('header', True) \
+        .mode('overwrite') \
         .csv(args.outputFilePath)
 except Exception as e: 
     logging.error(f"Unable to write output file")
